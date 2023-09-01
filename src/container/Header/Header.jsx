ca86305ae5eecf6cc9441c50e4e64187cb5ba4ea
Tipchan Songtheng <tipchans777@gmail.com>
2023-09-01 14:01:56 +0530

    add image and header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,8 +9,18 @@ const Header = () => (
     <div className="app__wrapper_info">
       <SubHeading title="chase the new  flavour" />
       <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque egestas
+        turpis quis urna volutpat lacinia at et lorem. Praesent quis commodo
+        nunc. Donec dictum finibus aliquam.
+      </p>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
     </div>
-    <div className="app__wrapper_img"></div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header img" />
+    </div>
   </div>
 );
 
