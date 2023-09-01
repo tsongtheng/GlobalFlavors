fbf6ec4a44de0b4c55e8cf321fdb810ffd0e6571
Tipchan Songtheng <tipchans777@gmail.com>
2023-09-01 13:46:20 +0530

    bug fix

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -5,7 +5,7 @@ import { images } from "../../constants";
 import "./Header.css";
 
 const Header = () => (
-  <div className="app__header app_wrapper section__padding" id="home">
+  <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
       <SubHeading title="chase the new  flavour" />
       <h1 className="app__header-h1">The Key to Fine Dining</h1>
