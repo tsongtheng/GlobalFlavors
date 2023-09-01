4dc484e360fcc6c9c0559532c5e1e2ae6440fc4c
Tipchan Songtheng <tipchans777@gmail.com>
2023-09-01 15:52:21 +0530

    change font, add media query

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -4,7 +4,7 @@ import { images } from "../../constants";
 
 const SubHeading = ({ title }) => (
   <div style={{ marginBottom: "1rem" }}>
-    <p className="p__cormorant">{title}</p>
+    <p className="p__baskerville">{title}</p>
     <img src={images.spoon} alt="spoon" className="spoon__img" />
   </div>
 );
