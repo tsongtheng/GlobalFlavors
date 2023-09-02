db2d1b945f2d8ace39b19ea852503e3e29fe6c4e
Tipchan Songtheng <tipchans777@gmail.com>
2023-09-02 14:54:07 +0530

    create AboutUs container, add new image

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,25 +1,25 @@
-import bg from '../assets/bg.png';
-import chef from '../assets/chef.png';
-import G from '../assets/G.png';
-import gallery01 from '../assets/gallery01.png';
-import gallery02 from '../assets/gallery02.png';
-import gallery03 from '../assets/gallery03.png';
-import gallery04 from '../assets/gallery04.png';
-import knife from '../assets/knife.png';
-import logo from '../assets/logo.png';
-import menu from '../assets/menu.png';
-import overlaybg from '../assets/overlaybg.png';
-import spoon from '../assets/spoon.svg';
-import welcome from '../assets/welcome.png';
-import findus from '../assets/findus.png';
-import laurels from '../assets/laurels.png';
-import award01 from '../assets/award01.png';
-import award02 from '../assets/award02.png';
-import award03 from '../assets/award03.png';
-import award05 from '../assets/award05.png';
-import sign from '../assets/sign.png';
-import quote from '../assets/quote.png';
-import globalflavors from '../assets/globalflavors.png';
+import bg from "../assets/bg.png";
+import chef from "../assets/chef.png";
+import G from "../assets/G.png";
+import gallery01 from "../assets/gallery01.png";
+import gallery02 from "../assets/gallery02.png";
+import gallery03 from "../assets/gallery03.png";
+import gallery04 from "../assets/gallery04.png";
+import spoon1 from "../assets/spoon1.png";
+import logo from "../assets/logo.png";
+import menu from "../assets/menu.png";
+import overlaybg from "../assets/overlaybg.png";
+import spoon from "../assets/spoon.svg";
+import welcome from "../assets/welcome.png";
+import findus from "../assets/findus.png";
+import laurels from "../assets/laurels.png";
+import award01 from "../assets/award01.png";
+import award02 from "../assets/award02.png";
+import award03 from "../assets/award03.png";
+import award05 from "../assets/award05.png";
+import sign from "../assets/sign.png";
+import quote from "../assets/quote.png";
+import globalflavors from "../assets/globalflavors.png";
 
 export default {
   bg,
@@ -29,7 +29,7 @@ export default {
   gallery02,
   gallery03,
   gallery04,
-  knife,
+  spoon1,
   logo,
   menu,
   overlaybg,
--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import React from "react";
 
-import './AboutUs.css';
+import { images } from "../../constants";
+import "./AboutUs.css";
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="g letter" />
+    </div>
+
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__baskerville">About Us</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+          egestas turpis quis urna volutpat lacinia at et lorem. Praesent quis
+          commodo nunc. Donec dictum finibus aliquam.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+
+      <div className="app__aboutus-content_spoon1 flex__center">
+        <img src={images.spoon1} alt="about_spoon1" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__baskerville">Our History</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+          egestas turpis quis urna volutpat lacinia at et lorem. Praesent quis
+          commodo nunc. Donec dictum finibus aliquam.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+    </div>
   </div>
 );
 
