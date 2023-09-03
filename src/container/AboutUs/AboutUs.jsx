f06cd77b879e355cec6438a019880b7adfb32402
Tipchan Songtheng <tipchans777@gmail.com>
2023-09-03 15:37:44 +0530

    update Aboutus section

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -4,15 +4,12 @@ import { images } from "../../constants";
 import "./AboutUs.css";
 
 const AboutUs = () => (
-  <div
-    className="app__aboutus app__bg flex__center section__padding"
-    id="about"
-  >
-    <div className="app__aboutus-overlay flex__center">
+  <div className="app__aboutus app__bg section__padding" id="about">
+    <div className="app__aboutus-overlay">
       <img src={images.G} alt="g letter" />
     </div>
 
-    <div className="app__aboutus-content flex__center">
+    <div className="app__aboutus-content">
       <div className="app__aboutus-content_about">
         <h1 className="headtext__baskerville">About Us</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
@@ -26,9 +23,9 @@ const AboutUs = () => (
         </button>
       </div>
 
-      <div className="app__aboutus-content_spoon1 flex__center">
+      {/* <div className="app__aboutus-content_spoon1 flex__center">
         <img src={images.spoon1} alt="about_spoon1" />
-      </div>
+      </div> */}
 
       <div className="app__aboutus-content_history">
         <h1 className="headtext__baskerville">Our History</h1>
