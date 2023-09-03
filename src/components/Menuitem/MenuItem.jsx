e02b7e3a3d4177475c64a81aacaa37627480551c
Tipchan Songtheng <tipchans777@gmail.com>
2023-09-03 21:09:48 +0530

    create SpecialMenu container, small update in Navbar & Header

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import './MenuItem.css';
+import "./MenuItem.css";
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = ({ title, price, tags }) => (
+  <div className="app__menuitem">
+    <div className="app__menuitem-head">
+      <div className="app__menuitem-name">
+        <p
+          className="p__baskerville"
+          style={{ color: "#c69749", fontStyle: "italic" }}
+        >
+          {title}
+        </p>
+      </div>
+      <div className="app__menuitem-dash" />
+      <div className="app__menuitem-price">
+        <p className="p__baskerville">{price}</p>
+      </div>
+    </div>
+
+    <div className="app__menuitem-sub">
+      <p
+        className="p__opensans"
+        style={{ color: "#545454", fontStyle: "italic" }}
+      >
+        {tags}
+      </p>
+    </div>
   </div>
 );
 
