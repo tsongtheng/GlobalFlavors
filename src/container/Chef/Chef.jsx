b49018e5890e76abe873d378c4902c4e61764adb
Tipchan Songtheng <tipchans777@gmail.com>
2023-09-04 18:23:51 +0530

    add new image, style chef container

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -5,22 +5,22 @@ import { images } from "../../constants";
 import "./Chef.css";
 
 const Chef = () => (
-  <div className="app__bg app__wrapper section__padding">
+  <div className="app__chef app__wrapper section__padding">
     <div className="app__wrapper_img app__wrapper_img-reverse">
       <img src={images.chef} alt="chef_image" />
     </div>
-    <div className="app__wrapper_info">
+    <div className="app__wrapper_info app__chef-info">
       <SubHeading title="Chef's word" />
-      <h1 className="headtext__cormorant">What we believe in</h1>
+      <h1 className="headtext__baskerville">What we believe in</h1>
 
       <div className="app__chef-content">
         <div className="app__chef-content_quote">
           <img src={images.quote} alt="quote_image" />
-          <p className="p__opensans">
+          <p className="p__opensans p__onpensans-chef_quote">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor sit .
           </p>
         </div>
-        <p className="p__opensans">
+        <p className="p__opensans p__onpensans-chef_quote">
           {" "}
           auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit.
           Nulla scelerisque scelerisque congue ac consequat, aliquam molestie
@@ -29,7 +29,7 @@ const Chef = () => (
       </div>
 
       <div className="app__chef-sign">
-        <p>Kevin Luo</p>
+        <p>Eric Adjepong</p>
         <p className="p__opensans">Chef & Founder</p>
         <img src={images.sign} alt="sign_image" />
       </div>
