8121872d8dcd09cb3a69502273d8596480acd75b
Tipchan Songtheng <tipchans777@gmail.com>
2023-09-05 17:49:16 +0530

    add attributes in video tag

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -4,10 +4,21 @@ import { BsFillPlayFill, BsPauseFill } from "react-icons";
 import { meal } from "../../constants";
 import "./Intro.css";
 
-const Intro = () => (
-  <div className="app__video">
-    <video src={meal} />
-  </div>
-);
+const Intro = () => {
+  const vidRef = React.useRef();
+
+  return (
+    <div className="app__video">
+      <video
+        src={meal}
+        ref={vidRef}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+    </div>
+  );
+};
 
 export default Intro;
