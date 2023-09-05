14c624494509de67432578500605ed5d4e388e8b
Tipchan Songtheng <tipchans777@gmail.com>
2023-09-05 17:29:30 +0530

    update chef container background

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -5,7 +5,7 @@ import { images } from "../../constants";
 import "./Chef.css";
 
 const Chef = () => (
-  <div className="app__chef app__wrapper section__padding">
+  <div className="app__chef-bg app__wrapper section__padding">
     <div className="app__wrapper_img app__wrapper_img-reverse">
       <img src={images.chef} alt="chef_image" />
     </div>
