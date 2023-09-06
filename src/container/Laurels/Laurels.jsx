117ad1ea719d0586efdce12c300204399995e57e
Tipchan Songtheng <tipchans777@gmail.com>
2023-09-06 15:33:23 +0530

    add award images

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -4,11 +4,25 @@ import { SubHeading } from "../../components";
 import { data, images } from "../../constants";
 import "./Laurels.css";
 
+const AwardCard = ({ award: { imgUrl, title, subtitle } }) => {
+  return (
+    <div className="app__laurels_awards-card">
+      <img src={imgUrl} alt="award" />
+    </div>
+  );
+};
+
 const Laurels = () => (
   <div className="app__wrapper section__padding" id="awards">
     <div className="app__wrapper_info">
       <SubHeading title="Awards & recognition" />
       <h1 className="headtext__baskerville">Our Laurels</h1>
+
+      <div className="app__laurels_awards">
+        {data.awards.map((award) => (
+          <AwardCard award={award} key={award.title} />
+        ))}
+      </div>
     </div>
     <div className="app__wrapper_img"></div>
   </div>
