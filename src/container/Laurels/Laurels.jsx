fdbb9d285b6c06b6bd17dfbc1147e56b0aa0fab2
Tipchan Songtheng <tipchans777@gmail.com>
2023-09-06 15:23:58 +0530

    create Laurels container

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import './Laurels.css';
+import { SubHeading } from "../../components";
+import { data, images } from "../../constants";
+import "./Laurels.css";
 
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className="app__wrapper section__padding" id="awards">
+    <div className="app__wrapper_info">
+      <SubHeading title="Awards & recognition" />
+      <h1 className="headtext__baskerville">Our Laurels</h1>
+    </div>
+    <div className="app__wrapper_img"></div>
   </div>
 );
 
