e18669a081633f012ca50905b7d37b3430b588b7
Tipchan Songtheng <tipchans777@gmail.com>
2023-09-08 00:13:38 +0530

    small className fix

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -57,7 +57,7 @@ const Gallery = () => {
         </div>
       </div>
 
-      <div className="app__gallery-images_arrow">
+      <div className="app__gallery-images_arrows">
         <BsArrowLeftShort
           className="gallery__arrow-icon"
           onClick={() => scroll("left")}
