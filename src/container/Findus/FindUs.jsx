640420163b1303afbff3d1ace812d8585522f23e
Tipchan Songtheng <tipchans777@gmail.com>
2023-09-09 23:06:04 +0530

    create Findus container

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React from "react";
+
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
 
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className="app__wrapper section__padding" id="contact">
+    <div className="app__wrapper_info">
+      <SubHeading title="Contact" />
+      <h1 className="headtext__baskerville">Find Us</h1>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.findus} alt="findus" />
+    </div>
   </div>
 );
 
