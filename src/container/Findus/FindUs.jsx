53e586592588d2427ae5c5aaf6a8c2b5af47d9ca
Tipchan Songtheng <tipchans777@gmail.com>
2023-09-09 23:28:06 +0530

    add inline style to Findus container

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -7,17 +7,26 @@ const FindUs = () => (
   <div className="app__wrapper section__padding" id="contact">
     <div className="app__wrapper_info">
       <SubHeading title="Contact" />
-      <h1 className="headtext__baskerville">Find Us</h1>
+      <h1 className="headtext__baskerville" style={{ marginBottom: "3rem" }}>
+        Find Us
+      </h1>
       <div className="app__wrapper-content">
         <p className="p__opensans">
           Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 7SG
         </p>
-        <p className="p__baskerville">Opening Hours</p>
+        <p
+          className="p__baskerville"
+          style={{ color: "#c69749", margin: "2rem 0" }}
+        >
+          Opening Hours
+        </p>
         <p className="p__opensans">Mon - Fri: 10:00 am - 02:00 am</p>
         <p className="p__opensans">Sat - Sun: 10:00 am - 03:00 am</p>
       </div>
 
-      <button className="custom__button">Visit Us</button>
+      <button className="custom__button" style={{ marginTop: "2rem" }}>
+        Visit Us
+      </button>
     </div>
 
     <div className="app__wrapper_img">
